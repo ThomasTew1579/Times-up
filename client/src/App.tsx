a06e9197d28cf50b637428e87a11de1e7a9a4078
Thomas <pasquet.thomas69@gmail.com>
2025-08-22 11:10:30 +0200

    fix: correction nom component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom'
-import './App.css'
 import Menu from './components/Menu'
 import MenuItem from './components/MenuItem'
 import Home from './pages/Home'
