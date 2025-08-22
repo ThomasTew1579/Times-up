178df585c43003ce5c198482e0936037c0f111db
Thomas <pasquet.thomas69@gmail.com>
2025-08-22 13:39:22 +0200

    fix: style Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,13 @@
-import CardHome from '../components/CardHome'
+import GameCard from '../components/GameCard'
 
 function Home() {
   return (
     <main className="mx-auto container px-4 py-8">
-      <h1 className="text-3xl text-center font-bold mb-6">Modes de jeu</h1>
+      <h1 className="text-3xl text-white text-center font-secondary font-bold mb-6">Modes de jeu</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        <CardHome href="/game/classic/setup" >
-          <h2 className="text-5xl text-primary-900 font-primary">Classique</h2>
-        </CardHome>
+        <GameCard href="/game/classic/setup" >
+          <h2 className="text-5xl text-stroke font-primary">Classique</h2>
+        </GameCard>
       </div>
     </main>
   )
